81fa1749bd83167b3d07597c1b7effbca3eb4f29
isrltuvi <tuvshinkhuu@gmail.com>
2017-06-24 17:44:40 +0900

    Add new post template.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from .forms import PostForm
+from django.utils import timezone
+from django.shortcuts import redirect
 
 # Create your views here.
 
 def post_list(request):
 	return render(request, 'blog/post_list.html', {})
+
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_list')
+	else:
+		form = PostForm()
+
+	return render(request, 'blog/post_edit.html', {'form':form})
